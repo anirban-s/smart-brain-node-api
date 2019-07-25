45103c9a8eb7047fb3d2332a2ce9eb8527c7d810
anirban-s <baban.chrome@gmail.com>
2019-07-26 02:41:49 +0530

    changing methods to suport register user and image count

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get('/', (req, res)=> {
 app.post('/signin', (req, res)=> {
   if(req.body.email === database.users[0].email &&
       req.body.password === database.users[0].password){
-    res.json('success');
+    res.json(database.users[0]);
   }
   else {
     res.status(400).json('error logging in');
@@ -43,12 +43,10 @@ app.post('/signin', (req, res)=> {
 
 app.post('/register', (req, res)=> {
   const {name, email, password} = req.body;
-
   database.users.push({
     id:'125',
     name: name,
     email: email,
-    password: password,
     entries: 0,
     joined: new Date()
   });
@@ -76,7 +74,7 @@ app.put('/image', (req,res)=>{
     if(user.id === id){
       found = true;
       user.entries++;
-      return res.json(user);
+      return res.json(user.entries);
     }
   });
   if(!found)
