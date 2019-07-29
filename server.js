ecdcdc2d45c019b69d33fa14241f3bdc09aaf524
anirban-s <baban.chrome@gmail.com>
2019-07-29 14:07:24 +0530

    adding procfile

--- server.js
+++ server.js
@@ -39,5 +39,5 @@ app.post('/imageUrl', (req, res)=> {image.handleAPICall(req,res)})
 
 
 app.listen(process.env.PORT || 3000, ()=>{
-  console.log('app is running on port ${process.env.PORT}');
+  console.log(`app is running on port ${process.env.PORT}`);
 })
